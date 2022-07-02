83cf84ed73d67dfc56e0b7ac9890d1dadf06ccff
Thisara Shyamalee <31173511+ShyamaleeT@users.noreply.github.com>
2022-07-02 20:58:08 +0530

    Update CNN Based Fundus Images Classification.py

--- CNN Based Fundus Images Classification.py
+++ CNN Based Fundus Images Classification.py
@@ -21,6 +21,11 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
+from skimage import io, filters, feature
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
+import cv2
+
 
 #set the train and test paths
 train_path = '/content/drive/MyDrive/RIM-ONE/train'
@@ -52,7 +57,51 @@ validation_generator = validation_datagen.flow_from_directory(
         classes=train_classes,
         class_mode='categorical')
 
-#train_crops = crop_generator(train_generator, 299)
+
+#Apply CLAHE and Dilation methods
+clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+clahe_img = clahe.apply(l)
+#plt.hist(clahe_img.flat, bins=100, range=(0,255))
+
+#Combine the CLAHE enhanced L-channel back with A and B channels
+updated_lab_img2 = cv2.merge((clahe_img,a,b))
+
+#Convert LAB image back to color (RGB)
+CLAHE_img = cv2.cvtColor(updated_lab_img2, cv2.COLOR_LAB2BGR)
+
+cv2_imshow(img)
+cv2_imshow(CLAHE_img)
+
+#Apply Dilation Method
+img = cv2.imread('image',0)
+_, mask = cv2.threshold(img, 255, 255, cv2.THRESH_BINARY_INV)
+
+kernal = np.ones((10,10), np.uint8)
+
+dilation = cv2.dilate(mask, kernal, iterations=2)
+
+titles = ['dilation']
+images = [dilation]
+plt.show()
+
+#Apply Data Augmentation Techniques
+train_datagen = ImageDataGenerator( rotation_range=10,
+                                    shear_range=0.2,
+                                    zoom_range=0.1,
+                                    horizontal_flip=True,
+                                    #vertical_flip=True,
+                                    width_shift_range=0.1,
+                                    height_shift_range=0.1)
+
+validation_datagen= ImageDataGenerator()
+
+
+
+
+
+
+
+
 
 train_generator.class_indices
 validation_generator.class_indices
